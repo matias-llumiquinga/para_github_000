fa002ea0ae2f686a2a4c692f90e92aaedaf7de5c
matias.llumiquinga <matias.llumiquinga@udla.edu.ec>
2024-09-30 13:50:39 -0500

    Inicio

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) 
+{
+    printf("Holaaa");
+    printf("Holaaa");
+    printf("Holaaa");
+    printf("Holaaa");
+    printf("Holaaa");
+    printf("Holaaawwwwwwwwwww");
+    printf("Holaaa");
+    return 0;
+}
